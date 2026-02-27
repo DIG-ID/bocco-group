9c85ac2cbd25221512a8ebf0d8ee64f46c448001
Pedro <pedro@dig.id>
2026-02-27 16:29:12 +0000

    up

--- header-webinar.php
+++ header-webinar.php
@@ -7,17 +7,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<meta name="theme-color" content="#080f26" />
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-F5VJG2ZFCC"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-
-			gtag('config', 'G-F5VJG2ZFCC');
-		</script>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TBL8RP8D');</script>
+		<!-- End Google Tag Manager -->
 		<?php wp_head(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBL8RP8D"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<?php do_action( 'wp_body_open' ); ?>
