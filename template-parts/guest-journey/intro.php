e1d4dd2260a6a91fc640d22f57ab6e516cd1b0d7
Pedro <pedro@dig.id>
2026-03-16 18:10:11 +0000

    up

--- template-parts/guest-journey/intro.php
+++ template-parts/guest-journey/intro.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12 col-md-12 col-lg-7 col-xl-7 section-gjintro__leftcol">
-				<div class="section-gjintro__content-wrapper" data-animate="fade-left" data-delay="0.2s">
+				<div class="section-gjintro__content-wrapper" data-animate="fade-left" data-animate-stagger="0.15">
 					<p class="section-gjintro__subtitle" data-animate-child><?php the_field( 'overview_subtitle' ); ?></p>
 					<h1 class="section-gjintro__title" data-animate-child><?php the_field( 'overview_title' ); ?></h1>
 					<p class="section-gjintro__text" data-animate-child><?php the_field( 'overview_description' ); ?></p>
